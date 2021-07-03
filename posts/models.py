99be460a66a02458984159d8cce0c5cfbdbb2d30
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-03 15:43:32 +0300

    add the database models into the app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+  loc_name = models.CharField(max_length=50)
+
+class category(models.Model):
+  cat_name = models.CharField(max_length=30)
+
+class ImagePost(models.Model):
+  image = models.ImageField(upload_to = 'posts/')
+  image_name = models.CharField(max_length=50)
+  image_description = models.CharField(max_length=200)
+  post_date= models.DateTimeField(auto_now_add = True)
+  location = models.ForeignKey(Location , on_delete = models.CASCADE, null=True)
+  category = models.ManyToManyField(category)
+
+  def __str__(self):
+    return self.image_name
+
+  def save_imagepost(self):
+    self.save()
+
+
+  
+
+  
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import category,Location,ImagePost
 
 # Create your tests here.
+
+class CategoryTest(TestCase):
+  def setUp(self):
+    '''
+    Creates an instance of the category model
+    '''
+    self.category = category(cat_name ='fashion')
+  
+  def test_instance(self):
+    '''
+    Test the instation of the category model
+    '''
+    self.assertTrue(isinstance(self.category,category))
+
+class LocationTest(TestCase):
+  def setUp(self):
+    '''
+    Creates an instance of the category model
+    '''
+    self.location = Location(loc_name ='Ukraine')
+  def test_instance(self):
+    '''
+    Test the instatioation of location model
+    '''
+    self.assertTrue(isinstance(self.location,Location))
+
+class CategoryTest(TestCase):
+  def setUp(self):
+    '''
+    Creates an instance of the ImagePost model
+    '''
+    self.imagepost = ImagePost(image ='mango.jpeg', image_name='Mountains', 
+    image_description= 'This is beautiful')
+  
+  def test_instance(self):
+    '''
+    Test the instation of the ImagePost model
+    '''
+    self.assertTrue(isinstance(self.imagepost,ImagePost))
+  
+  def test_save_imagepost(self):
+    '''
+    Test save imagepost method
+    '''
+    self.imagepost.save_imagepost()
+    posts = ImagePost.objects.all()
+    self.assertTrue(len(posts) > 0)
+
