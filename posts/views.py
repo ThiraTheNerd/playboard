5ba11db61bf31310b9a1861fc615d98489946dc2
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-03 12:54:54 +0300

    add bootstrap,css and navbar

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  '''
+  Function that displays the homepage of the app
+  '''
+  return render(request, 'index.html')
