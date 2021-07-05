16b085030661b2d975528b7f83b043095a34653a
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-05 15:41:02 +0300

    change the categories setup

--- posts/admin.py
+++ posts/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import ImagePost,category,Location
 # Register your models here.
 
-class ImagePostAdmin(admin.ModelAdmin):
-  filter_horizontal =('category',)
-
 admin.site.register(Location)
 admin.site.register(category)
-admin.site.register(ImagePost, ImagePostAdmin)
+admin.site.register(ImagePost)
