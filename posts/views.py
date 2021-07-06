a63fd34b6e32c557d728cc3d3d1127ab21962b9f
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-06 12:05:31 +0300

    add the error exception hander

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.core.checks import messages
+from django.http.response import Http404
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from .models import ImagePost, category, Location
 # Create your views here.
@@ -14,12 +16,13 @@ def index(request):
 def search_results(request):
   if 'category' in request.GET and request.GET["category"]:
     search_term = request.GET.get('category')
-    cat_name = category.objects.get(cat_name = search_term)
-    cat_id = cat_name.id
-
-    searched_categories = ImagePost.search_category(cat_id)
-    message = f"{search_term}"
-
+    try:
+      cat_name = category.objects.get(cat_name = search_term)
+      cat_id = cat_name.id
+      searched_categories = ImagePost.search_category(cat_id)
+      message = f"{search_term}"
+    except ObjectDoesNotExist:
+      raise Http404()
     return render(request, 'search.html', {"message": message, "categories":searched_categories })
   else:
     message = "You haven't searched for any term"
