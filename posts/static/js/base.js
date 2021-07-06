01799eefbddae69dda92e0bfacedd983a2cf7d83
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-06 10:44:26 +0300

    implement the javascript code to copy to clipboard

--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -1,10 +1,6 @@
 console.log('Hello World')
 
-const copyBtns = [...document.getElementsByClassName('copy-btn')]
-console.log(copyBtns)
-
-function selfCopy(copyText)
-{
-  navigator.clipboard.writeText(copyText);
-  alert('THIS BUTTON HAS BEEN CLICKED')
+copyLink = (element) => {
+  document.getElementById(element).select();
+  document.execCommand("copy")
 }
